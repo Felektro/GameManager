727af8b2dd48bf5594089f2a4326cce0cfb14d8a
BkillerCold <guy.nisanov@gmail.com>
2026-04-29 12:55:41 -0400

    Added some classes

---
+++ GameLauncher.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class GameLauncher here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameLauncher
+{
+    double startTime;
+    double endTime;
+    public GameLauncher()
+    {
+        
+    }
+    
+    public void startTime()
+    {
+        
+    }
+    
+    public void endTime()
+    {
+        
+    }
+}
---
+++ MultiPlayer.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class MultiPlayer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MultiPlayer extends Game
+{
+    int minPlayer;
+    int maxPlayer;
+    public MultiPlayer(String gameName, String platform, char gameAgeRate)
+    {
+        super(gameName, platform, gameAgeRate);
+    }
+}
